88614d7af6ac7569bd555f35178674d02b9e87e8
Dolan Murvihill <dmurvihill@gmail.com>
2013-12-19 15:27:23 -0500

    Added Oracle class, and several event classes; added the capability to register event listeners with links and sending agents.

--- simple_sim.py
+++ simple_sim.py
@@ -23,6 +23,7 @@ class Master_link:
         self.recving_link.tick()
 
 class Link:
+    # TODO P2 call listeners when send, receive, enqueue, or drop events occur
     # takes a dict of link address pairs
     def __init__(self,queue_length,agent_side):
         self.agent_side = agent_side # this is a boolean flag that determines if the link is connected to the sending agents or not for purposes of addressing
@@ -31,6 +32,12 @@ class Link:
 #        for key in self.addr_link_pairs:
 #            self.addr_buffer_pairs[key] = []
         self.queue_length = queue_length
+
+        # Routines to call when certain events occur
+        self.enQ_events = []
+        self.send_events = []
+        self.recv_events = []
+        self.drop_events = []
 
     def register(self,addr,link):
         self.addr_link_pairs[addr] = link
@@ -69,7 +76,20 @@ class Link:
         bpairs[addr] += packets
         bpairs[addr] = bpairs[addr][:self.queue_length]
 
+    def on_send(routine):
+        self.send_events.append(routine)
+
+    def on_recv(routine):
+        self.recv_events.append(routine)
+
+    def on_enQ(routine):
+        self.recv_events.append(routine)
+
+    def on_drop(routine):
+        self.drop_events.append(routine)
+
 class sending_agent:
+    # TODO P2 call listeners when success events occur
     def __init__(self,link,cc,addr):
         self.link = link
         self.cc = cc # The congestion control algorithm to use
@@ -83,6 +103,8 @@ class sending_agent:
         self.bandwidth = 3 # default bandwidth
         self.mission = 45 # default number of packets to be sent 
 
+        self.success = [] # Routine to call when we receive a valid ACK
+
     def tick(self):
         self.handle_input(self.link.recv(self.bandwidth,self.host_addr))
         self.link.send(self.goal_addr,self.generate_packets())
@@ -95,6 +117,9 @@ class sending_agent:
 
     def generate_packets(self):
         return self.cc.get_pending_sends()
+
+    def on_success(routine):
+        self.success.append(routine)
 
 class recving_agent:
     def __init__(self,link,addr):
@@ -126,6 +151,74 @@ class recving_agent:
     def generate_packets(self):
         # ACK the last in-order packet
         return [self.last_in_order_seq]
+
+# The Oracle registers listeners with the various network objects and is thus 
+# able to keep detailed logs to track what is going on in the network.
+class Oracle:
+    def __init__(self):
+        self.events = []
+    
+    # Start listening for events on a link
+    def reg_link(self, link):
+        link.on_drop(self.drop)
+        link.on_send(self.send)
+        link.on_recv(self.recv)
+        link.on_enQ(self.enQ)
+
+    # Start listening for events on an agent
+    def reg_agt(self, agt):
+        agt.on_success(self.success)
+    
+    def drop(self, time, obj, seqno, src, dst):
+        self.events.append(DropEvent(time, obj, seqno, src, dst))
+        print "Drop!"
+
+    def enQ(self, time, src, dst, seqno):
+        self.events.append(EnQEvent(time,src,seqno,dst))
+    
+    def success(self, time, src, dst, seqno, sent):
+        self.events.append(TransmitSuccessEvent(time,src,seqno,dst,sent))
+
+    def send(self, time, src, dst, seqno):
+        self.events.append(SendEvent(time, src, seqno, dst))
+
+    def recv(self, time, src, dst, seqno):
+        self.events.append(RecvEvent(time, dst, seqno, src))
+
+# Base class for simulation events. DO NOT INSTANTIATE.
+class SimEvent:
+    def __init__(self, time, obj, seqno):
+        self.time = time # simulation time the event was reported
+        self.obj = obj # the simulation object reporting the event
+        self.seqno = seqno # sequence number of packet
+    
+class EnQEvent(SimEvent):
+    def __init__(self, time, obj, seqno):
+        SimEvent.__init__(self, time, obj, seqno)
+
+class SendEvent(SimEvent):
+    def __init__(self, time, src, seqno, dst):
+        SimEvent.__init__(self, time, src, seqno)
+        self.dst = dst
+
+class RecvEvent(SimEvent):
+    def __init__(self, time, dst, seqno, src):
+        SimEvent.__init__(self, time, dst, seqno)
+        self.src = src
+
+class DropEvent(SimEvent):
+    def __init__(self, time, obj, seqno, src, dst):
+        SimEvent.__init__(self, time, obj, seqno)
+        self.src = src
+        self.dst = dst
+
+# Signifies the receipt of an acknowledgment by a sender
+class TransmitSuccessEvent(SimEvent):
+    def __init__(self, time, src, seqno, dst, sent):
+        SimEvent.__init__(self, time, src, seqno)
+        self.dst = dst
+        self.sent = sent # Simulation time the packet was sent
+        self.rtt = time - sent # Round trip time for this packet
 
 def main(argv=None):
     if argv==None:
